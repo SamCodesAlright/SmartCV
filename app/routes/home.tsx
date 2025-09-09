bd4fa88319e1e60777c94d5e51c6ed21844622a5
Sameer Shaikh <sameeritis10@gmail.com>
2025-09-09 23:32:14 +0530

    Code to display the uploaded resume on home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "../../constants";
 import ResumeCard from "../components/ResumeCard";
 import { usePuterStore } from "~/lib/puter";
-import { useNavigate } from "react-router";
-import { useEffect } from "react";
+import { Link, useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -27,12 +26,30 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function home() {
-  const { auth } = usePuterStore();
+  const { auth, kv } = usePuterStore();
   const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResume, setLoadingResume] = useState(false);
 
   useEffect(() => {
     if (!auth.isAuthenticated) navigate("/auth?next=/");
   }, [auth.isAuthenticated]);
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      setLoadingResume(true);
+      const resumes = (await kv.list("resume:*", true)) as KVItem[];
+
+      const parsedResumes = resumes?.map(
+        (resume) => JSON.parse(resume.value) as Resume
+      );
+
+      console.log(parsedResumes);
+      setResumes(parsedResumes || []);
+      setLoadingResume(false);
+    };
+    loadResumes();
+  }, []);
 
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -40,14 +57,38 @@ export default function home() {
       <section className="main-section">
         <div className="page-heading py-16">
           <h1>Track Your Applications and Resume Ratings</h1>
-          <h2>Review you submissions and check AI-Powered Feedback</h2>
+          {!loadingResume && resumes.length === 0 ? (
+            <h2>
+              No resumes found. Upload a resume to start tracking your job
+              applications.
+            </h2>
+          ) : (
+            <h2>Review you submissions and check AI-Powered Feedback</h2>
+          )}
         </div>
 
-        {resumes.length > 0 && (
+        {loadingResume && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+        )}
+
+        {!loadingResume && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCard key={resume.id} resume={resume} />
             ))}
+          </div>
+        )}
+
+        {!loadingResume && resumes.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link
+              to="/upload"
+              className="primary-button w-fit text-xl font-semibold"
+            >
+              Upload Resume
+            </Link>
           </div>
         )}
       </section>
